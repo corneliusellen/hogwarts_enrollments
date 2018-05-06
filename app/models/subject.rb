543035209dac3fabd0f071687e8abfa6a3d3a0e1
Ellen Cornelius <ecorneli@iwu.edu>
2018-05-05 19:15:45 -0600

    Write model methods.

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -2,4 +2,14 @@ class Subject < ApplicationRecord
   belongs_to :teacher
   has_many :enrollments
   has_many :students, through: :enrollments
+
+  def self.subjects_with(house_name)
+  	joins(:students)
+  	.distinct
+    .where(students: {house: house_name})
+  end
+
+  def breakdown_students
+  	students.group(:house).count
+  end
 end
--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,3 +1,12 @@
 class Teacher < ApplicationRecord
   has_many :subjects
+
+  def self.lowest_class_average_with(house)
+    select("teachers.*, avg(enrollments.grade) AS average_grade")
+    .joins(subjects: [enrollments: :student])
+    .where(students: {house: house})
+    .group(:id, :subject_id)
+    .order("average_grade")
+    .first
+  end
 end
